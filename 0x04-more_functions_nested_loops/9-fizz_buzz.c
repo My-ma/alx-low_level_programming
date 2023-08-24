33bab60279f94661fedb23b029431a3d47a240e7
My-ma <jem.lutterodt@gmail.com>
2023-08-24 09:33:25 -0700

    fuzz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - function print the numbers from 1-100, but for multiples of three,
- * 	Fizz is printed instead for multiples of five,
- * 	Buzz is printed for multiples of both three and five, FizzBuzz.
+ * Fizz is printed instead for multiples of five,
+ * Buzz is printed for multiples of both three and five, FizzBuzz.
  *
  * Return: Always 0.
  */
