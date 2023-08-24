59d8740741af1e0c8a4736d814492239006f8eeb
My-ma <jem.lutterodt@gmail.com>
2023-08-24 05:51:59 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function check for a digit (0 to 9)
+ *
+ * Return: Always 0
+ */
+void  print_most_numbers()
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		if (j != 50)
+		{
+			if (j != 52)
+			{
+				_putchar(j);
+			}
+		}
+	}
+	_putchar('\n');
+}
